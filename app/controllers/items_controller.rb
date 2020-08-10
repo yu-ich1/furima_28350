6b51d4be107aa4dd12c0b6d469ed96a82e8f7ae3
yu-ich1 <68319482+yu-ich1@users.noreply.github.com>
2020-08-10 19:07:40 +0900

    ログイン状況でアクセス変更実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,16 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  # before_action :move_to_index, except: [:index]
+  before_action :authenticate_user!, except: [:index]
+
 
   def index
   end
 
   def new
+    
   end
 
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
-     end
+  # def move_to_index
+  #   redirect_to action: :index unless user_signed_in?
+  #    end
 end
