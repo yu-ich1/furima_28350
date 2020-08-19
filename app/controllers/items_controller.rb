a8e22ffc764887d2a227ea3820fa1608bd808d54
yu-ich1 <68319482+yu-ich1@users.noreply.github.com>
2020-08-19 14:13:36 +0900

    itemアクションの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,7 +26,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
