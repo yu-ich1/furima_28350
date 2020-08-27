9120ed604b9cba2fae6a383c47f1cda1e4e897ec
yu-ich1 <68319482+yu-ich1@users.noreply.github.com>
2020-08-27 16:51:53 +0900

    微調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,6 +14,6 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :product_name, :explanation, :image
     validates :category_id, :product_status_id, :shipping_charges_id, :shipping_origin_id, :date_of_shipment_id, numericality: { other_than: 1 }
-    validates :product_price, numericality: { greater_than: 300, less_than_or_equal_to: 9_999_999, message: '価格の範囲は、¥300~¥9,999,999の間です' }
+    validates :product_price, numericality: { greater_than: 299, less_than_or_equal_to: 9_999_999, message: '価格の範囲は、¥300~¥9,999,999の間です' }
   end
 end
